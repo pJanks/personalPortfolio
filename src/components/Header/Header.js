880a0134a840184e1accd98dd2278ce0f19ad1f1
Johnny Cassidy <johnny.k.cassidy@gmail.com>
2021-01-13 13:48:38 -0800

    Break styles out into separate file

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,46 +1,9 @@
-import styled from 'styled-components'
 import { resumeData } from '../../resumeData'
+import { StyledHeaderWrapper, StyledName, StyledJobTitle, StyledSocialMediaWrapper, StyledIcons } from './HeaderStyledComponents'
 import gitHubIcon from '../../images/github_icon.png'
 import twitterIcon from '../../images/twitter_icon.png'
 import linkedInIcon from '../../images/linkedin_icon.png'
 import downArrowIcon from '../../images/down_arrow_icon.png'
-
-const StyledHeaderWrapper = styled.section`
-  align-items: center;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  width: 100%;
-`
-
-const StyledName = styled.h1`
-  color: #D8F3DC;
-  font-size: 500%;
-  margin: 4% 0 2% 0;
-  text-align: center;
-`
-
-const StyledJobTitle = styled.h3`
-  color: #D8F3DC;
-  font-size: 150%;
-  text-align: center;
-`
-
-const StyledSocialMediaWrapper = styled.div`
-  display: flex;
-  justify-content: center;
-  width: 30%;
-`
-
-const StyledIcons = styled.img`
-  height: 50px;
-  margin: 13vh 3vw 0 3vw;
-  width: 50px;
-
-  &:hover {
-    cursor: pointer;
-  }
-`
 
 export const Header = () => {
   return (
