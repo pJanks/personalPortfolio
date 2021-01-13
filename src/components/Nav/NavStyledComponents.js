dca3aca5e59c0b49f23c6389980b58f5def48d71
Johnny Cassidy <johnny.k.cassidy@gmail.com>
2021-01-13 14:45:10 -0800

    Break out styles into separate file

---
+++ src/components/Nav/NavStyledComponents.js
@@ -0,0 +1,23 @@
+import styled from 'styled-components'
+
+export const StyledNavWrapper = styled.nav`
+  align-items: center;
+  background-color: #74C69D;
+  display: flex;
+  justify-content: center;
+  width: 100%;
+`
+export const StyledNavButton = styled.button`
+  background-color: #74C69D;
+  border: none;
+  border-radius: 20%;
+  color: #FFF;
+  font-size: 125%;
+  font-weight: bold;
+  margin: 1% 2% 1% 2%;
+  padding: 1% 2% 1% 2%;
+
+  &:hover {
+    background-color: #52B788;
+  }
+`
