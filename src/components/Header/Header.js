1493736d1ca47e6eb099205668a542d5fdcd9880
Johnny Cassidy <johnny.k.cassidy@gmail.com>
2021-01-13 15:07:09 -0800

    Update naming conventions

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,5 +1,5 @@
 import { resumeData } from '../../resumeData'
-import { StyledHeaderWrapper, StyledName, StyledJobTitle, StyledSocialMediaWrapper, StyledIcons } from './HeaderStyledComponents'
+import { StyledHeaderWrapper, StyledName, StyledJobTitle, StyledSocialMediaWrapper, StyledIcon } from './HeaderStyledComponents'
 import gitHubIcon from '../../images/github_icon.png'
 import twitterIcon from '../../images/twitter_icon.png'
 import linkedInIcon from '../../images/linkedin_icon.png'
@@ -11,11 +11,11 @@ export const Header = () => {
       <StyledName>Johnny Cassidy</StyledName>
       <StyledJobTitle>Software Developer</StyledJobTitle>
       <StyledSocialMediaWrapper>
-        <a rel='noopener noreferrer' aria-label='my github profile' href={resumeData.gitHubURL} target='_blank'><StyledIcons alt='Github icon' src={gitHubIcon} className='social-icons'/></a>
-        <a rel='noopener noreferrer' aria-label='my linkedin profile' href={resumeData.linkedInURL} target='_blank'><StyledIcons alt='Linkedin icon' src={linkedInIcon} className='social-icons'/></a>
-        <a rel='noopener noreferrer' aria-label='my twitter profile' href={resumeData.twitterURL} target='_blank'><StyledIcons alt='Twitter icon' src={twitterIcon} className='social-icons'/></a>
+        <a rel='noopener noreferrer' aria-label='my github profile' href={resumeData.gitHubURL} target='_blank'><StyledIcon alt='Github icon' src={gitHubIcon} className='social-icons'/></a>
+        <a rel='noopener noreferrer' aria-label='my linkedin profile' href={resumeData.linkedInURL} target='_blank'><StyledIcon alt='Linkedin icon' src={linkedInIcon} className='social-icons'/></a>
+        <a rel='noopener noreferrer' aria-label='my twitter profile' href={resumeData.twitterURL} target='_blank'><StyledIcon alt='Twitter icon' src={twitterIcon} className='social-icons'/></a>
       </StyledSocialMediaWrapper>
-      <StyledIcons alt='Next section icon' src={downArrowIcon} tabIndex='0'/>
+      <StyledIcon alt='Next section icon' src={downArrowIcon} tabIndex='0'/>
     </StyledHeaderWrapper>
   )
 }
