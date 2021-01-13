1e36fc8a6538645163868ac2393ba07818356909
Johnny Cassidy <johnny.k.cassidy@gmail.com>
2021-01-13 12:49:01 -0800

    Add file for resume info

---
+++ src/resumeData.js
@@ -0,0 +1,61 @@
+export const resumeData = {
+  resumeURL: 'https://alumni.turing.io/sites/default/files/resumes/Zachary_Nemeroff_resume_2020_0.pdf',
+  turingAlumniURL: 'https://alumni.turing.io/alumni/johnny-cassidy',
+  gitHubURL: 'https://github.com/pJanks',
+  twitterURL: 'https://twitter.com/pJanksio',
+  linkedInURL: 'https://www.linkedin.com/in/johnnycassidy',
+  bioDescription: 'My name is Johnny Cassidy and I am a JavaScript developer with a heavy emphasis on frontend development and functionality. I enjoy the benefits of test driven development and object-oriented programming combined with agile workflow practices. I have built many accessible applications consuming RESTful APIs and augmented with robust testing suites. My main focus in the past has been using JavaScript, HTML5, CSS3, jQuery, React.js, and React Native which I am most comfortable with. I am currently trying to layer Next.js on top of React in order to continue building my skills and keep my fingers on my keyboard.',
+  education: {
+    school: 'Turing School of Software & Design',
+    position: 'Graduate Front End Web Development Program',
+    time: 'September 2019 - April 2020',
+    description: [
+      'Built 14 unique web applications from scratch with vanilla JS, React.js, & React Native',
+      'Created thorough unit testing suites for every application',
+      'Programmed for team projects of four people, paired projects, and solo projects',
+      'Focused on JavaScript fundamentals for 12 weeks',
+      'Completed 1,200 hours building applications with front end and back end technologies',
+      'Dedicated 600 hours working strictly with React.js, React Native, Router, and Redux'
+    ]
+  },
+  jobs: [
+    {
+      name: 'Legal Thunder',
+      position: 'Frontend Developer (remote)',
+      time: 'August 2020 - November 2020',
+      description: [
+        'Create an MVP for use by legal professionals',
+        'Implement Stripe for processing e-commerce payments for subscriptions',
+        'Expand on an existing codebase and refactor code to more modern solutions for global state management (React Hooks vs Redux) as well as for styling (styled-components vs .css/.scss files)',
+        'Work on a fullstack team to collectively solve problems and build bulletproof code'
+      ]
+    },
+    {
+      name: 'Pabco Gypsum',
+      position: 'Warehouse Lead/Inventory Specialist',
+      time: 'April 2013 - September 2019',
+      description: [
+        'Train and direct a team of 15 employees',
+        'Count all board averaging about 30,000,000 sq. ft. of inventory and compare with filled orders to analyze completion percentage and confirm accuracy of shipments',
+        'Decide what products to run depending on current open orders and current inventory'
+      ]
+    },
+  ],
+  techSkills: [
+    'JavaScript',
+    'HTML5',
+    'CSS3',
+    'Styled-Components',
+    'React.js',
+    'React Native',
+    'React Router',
+    'jQuery',
+    'Redux',
+    'TravisCI',
+    'SCSS / SASS',
+    'Jest / Enzyme',
+    'Mocha / Chai',
+    'NPM / Yarn',
+    'Git / GitHub'
+  ]
+}
