bcb92a98b5ae97294e0d44e58d59e77ea46a530a
Johnny Cassidy <johnny.k.cassidy@gmail.com>
2021-01-13 14:44:58 -0800

    Break out styles into separate file

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,32 +1,11 @@
-import styled from 'styled-components'
+import { StyledNavWrapper, StyledNavButton } from './NavStyledComponents'
 
-const StyledNavWrapper = styled.nav`
-  align-items: center;
-  background-color: #74C69D;
-  display: flex;
-  justify-content: center;
-  width: 100%;
-`
-const StyledNavButton = styled.button`
-  background-color: #74C69D;
-  border: none;
-  border-radius: 20%;
-  color: #FFF;
-  font-size: 125%;
-  font-weight: bold;
-  margin: 1% 2% 1% 2%;
-  padding: 1% 2% 1% 2%;
-
-  &:hover {
-    background-color: #52B788;
-  }
-`
+export const Nav = () => {
 
 const handleButtonClicks = (e) => {
   console.log(e.target.innerText)
 }
 
-export const Nav = () => {
   return (
     <StyledNavWrapper>
       <StyledNavButton onClick={(e) => handleButtonClicks(e)}>Home</StyledNavButton>
