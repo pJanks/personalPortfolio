0f411c544685146b654c16b3738f5b5313da8843
Johnny Cassidy <johnny.k.cassidy@gmail.com>
2021-01-13 15:06:49 -0800

    Update imports

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import './App.css'
+import styled from 'styled-components'
 import { Header } from './components/Header/Header'
 import { Nav } from './components/Nav/Nav'
+import { About } from './components/About/About'
+
+const StyledMainContainer = styled.main`
+  height: 100vh;
+  width: 100vw;
+`
 
 const App = () => {
   return (
     <div>
-      <Nav />
-      <Header />
+      <StyledMainContainer>
+        <Nav />
+        <Header />
+      </StyledMainContainer>
+      <About />
     </div>
   )
 }
