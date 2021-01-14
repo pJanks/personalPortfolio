aeb0232a47536c17d40215f603a185c046f9351b
Johnny Cassidy <johnny.k.cassidy@gmail.com>
2021-01-13 19:48:27 -0800

    Adjust styles

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,9 +5,13 @@ import twitterIcon from '../../images/twitter_icon.png'
 import linkedInIcon from '../../images/linkedin_icon.png'
 import downArrowIcon from '../../images/down_arrow_icon.png'
 
+const performSmoothScroll = () => {
+  document.querySelector('#about').scrollIntoView({behavior: 'smooth'});
+}
+
 export const Header = () => {
   return (
-    <StyledHeaderWrapper>
+    <StyledHeaderWrapper id='home'>
       <StyledName>Johnny Cassidy</StyledName>
       <StyledJobTitle>Software Developer</StyledJobTitle>
       <StyledSocialMediaWrapper>
@@ -15,7 +19,7 @@ export const Header = () => {
         <a rel='noopener noreferrer' aria-label='my linkedin profile' href={resumeData.linkedInURL} target='_blank'><StyledIcon alt='Linkedin icon' src={linkedInIcon} className='social-icons'/></a>
         <a rel='noopener noreferrer' aria-label='my twitter profile' href={resumeData.twitterURL} target='_blank'><StyledIcon alt='Twitter icon' src={twitterIcon} className='social-icons'/></a>
       </StyledSocialMediaWrapper>
-      <StyledIcon alt='Next section icon' src={downArrowIcon} tabIndex='0'/>
+      <StyledIcon alt='Next section icon' onClick={performSmoothScroll} src={downArrowIcon} tabIndex='0'/>
     </StyledHeaderWrapper>
   )
 }
