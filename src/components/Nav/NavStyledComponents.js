de3e5a162eaece81fdec50a72a7c16ecdd30fc51
Johnny Cassidy <johnny.k.cassidy@gmail.com>
2021-01-13 16:14:04 -0800

    Adjust font styles

--- src/components/Nav/NavStyledComponents.js
+++ src/components/Nav/NavStyledComponents.js
@@ -4,18 +4,17 @@ export const StyledNavWrapper = styled.nav`
   align-items: center;
   background-color: #74C69D;
   display: flex;
+  height: 70px;
   justify-content: center;
   width: 100%;
 `
 export const StyledNavButton = styled.button`
   background-color: #74C69D;
   border: none;
-  border-radius: 20%;
-  color: #FFF;
+  color: #081C15;
   font-size: 125%;
   font-weight: bold;
-  margin: 1% 2% 1% 2%;
-  padding: 1% 2% 1% 2%;
+  padding: 2% 4% 2% 4%;
 
   &:hover {
     background-color: #52B788;
