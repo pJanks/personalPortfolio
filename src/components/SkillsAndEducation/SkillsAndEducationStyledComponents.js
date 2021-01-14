868ec21843efe265a0820d385516b661e5506a4a
Johnny Cassidy <johnny.k.cassidy@gmail.com>
2021-01-14 10:40:34 -0800

    Add styles for skills and education page

---
+++ src/components/SkillsAndEducation/SkillsAndEducationStyledComponents.js
@@ -0,0 +1,61 @@
+import styled from 'styled-components'
+
+export const StyledWrapper = styled.section`
+  background-color: #95D5B2;
+  display: flex;
+  line-height: 170%;
+  padding: 1%;
+  width: 100%;
+`
+
+export const StyledHeader = styled.h3`
+  background-color: #95D5B2;
+  font-size: 200%;
+  margin: 3%;
+  padding: 3%;
+  text-decoration: underline;
+`
+
+export const StyledUnorderedList = styled.ul`
+  background-color: #95D5B2;
+  margin-top: 6%;
+  width: 60%;
+`
+
+export const StyledListItem = styled.li`
+  background-color: #95D5B2;
+  font-size: 140%;
+  font-weight: bold;
+  margin-left: 30%;
+`
+
+export const StyledSchoolUnorderedList = styled.ul`
+  background-color: #95D5B2;
+  margin-top: 3%;
+  width: 100%;
+`
+
+export const StyledSchoolListItem = styled.li`
+  background-color: #95D5B2;
+  font-size: 140%;
+  font-weight: bold;
+`
+
+export const StyledSchoolName = styled.h4`
+  background-color: #95D5B2;
+  font-size: 160%;
+`
+
+export const StyledSchoolInfo = styled.span`
+  background-color: #95D5B2;
+  font-size: 100%;
+  font-style: italic;
+  font-weight: bold;
+`
+
+export const StyledSchoolContainer = styled.section`
+  background-color: #95D5B2;
+  display: flex;
+  flex-direction: column;
+  margin: 6% 3% 3% 3%;
+`
